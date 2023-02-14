bf530ae3ad2a4d20bbd6e358343b585bee00cfc1
Mark Pekala <mpekala@college.harvard.edu>
2023-02-14 00:46:43 -0500

    auth

---
+++ src/components/input.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+interface Props {
+  onChange: (val: string) => void;
+  className?: string;
+}
+
+export default function Input({ onChange, className }: Props) {
+  const [value, setValue] = useState("");
+
+  return (
+    <input
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      className={`${
+        className || ""
+      } w-full my-2 p-2 text-xl border border-slate-900`}
+    />
+  );
+}
---
+++ src/pages/signup.tsx
@@ -0,0 +1,37 @@
+import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
+import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
+import { useEffect, useState } from "react";
+import Slink from "@/components/slink";
+import { useRouter } from "next/router";
+
+export default function SignupPage() {
+  const supabaseClient = useSupabaseClient();
+  const user = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.replace("home");
+    }
+  }, [user, router]);
+
+  return (
+    <div>
+      <p className="text-xl">Sign up</p>
+      <Auth
+        view="sign_up"
+        redirectTo="http://localhost:3000/test"
+        appearance={{ theme: ThemeSupa }}
+        supabaseClient={supabaseClient}
+        providers={
+          [
+            /*"google", "github"*/
+          ]
+        }
+        socialLayout="horizontal"
+        showLinks={false}
+      />
+      <Slink href="signin">Already have an account? Sign in</Slink>
+    </div>
+  );
+}
