27a5f3a6c47d5f583bfbf087b84fb562fbf4e7ea
Mark Pekala <mpekala@college.harvard.edu>
2023-02-17 10:42:52 -0500

    basic seeding

---
+++ supabase/seed.sql
@@ -0,0 +1,58 @@
+select * from auth.users;
+
+insert into
+    auth.users (
+        id,
+        email,
+        encrypted_password,
+        email_confirmed_at
+    )
+values
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa00',
+        'mark@pekala.com',
+        '$2a$10$DFbvnrXWfmB8PgzonRl3eu0i3nxXjRrVPWtt5Yl0uqmTf7zLQ7eZq',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa02',
+        'chris@wirth.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa03',
+        'siri@prasad.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa04',
+        'david@hacker.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa05',
+        'michael@frim.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa06',
+        'raj@mittal.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa07',
+        'troy@powell.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    ),
+    (
+        '6148d9ae-9687-4434-804d-a3c4d370fa08',
+        'vassilios@kaxiras.com',
+        '$2a$10$5ijpbk9hVmO5Luy/vTObj.3dZS1XQKGzzWTsiixha.xD3wmqKJ1ZS',
+        '2023-02-17 04:32:46.892482+00'
+    );
